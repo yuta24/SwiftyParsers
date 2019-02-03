e5b55bb4ee682f71488d4b503166587a5f862cbc
yuta24 <yuta24@bivre.com>
2019-02-03 13:47:31 +0900

    Impl several parsers

--- Sources/SwiftyParsers/Character.swift
+++ Sources/SwiftyParsers/Character.swift
@@ -7,8 +7,24 @@ func separete(_ xs: String) -> (Character, String)? {
     }
 }
 
+func isOctDigit(_ c: Character) -> Bool {
+    return ("0"..."7").contains(c)
+}
+
+func isHexDigit(_ c: Character) -> Bool {
+    return ("a"..."f").contains(c) || ("A"..."F").contains(c)
+}
+
 func isDigit(_ c: Character) -> Bool {
     return ("0"..."9").contains(c)
+}
+
+func isAlpha(_ c: Character) -> Bool {
+    return isLower(c) || isUpper(c)
+}
+
+func isAlphaNum(_ c: Character) -> Bool {
+    return isAlpha(c) || isDigit(c)
 }
 
 func isLower(_ c: Character) -> Bool {
@@ -19,6 +35,12 @@ func isUpper(_ c: Character) -> Bool {
     return ("A"..."Z").contains(c)
 }
 
+func isChar(_ c: Character) -> (Character) -> Bool {
+    return {
+        return c == $0
+    }
+}
+
 public func satisfy(_ condition: @escaping (Character) -> Bool) -> Parser<Character> {
     return Parser(parse: { (xs) -> (Character, String)? in
         if let result = separete(xs), condition(result.0) {
@@ -26,6 +48,12 @@ public func satisfy(_ condition: @escaping (Character) -> Bool) -> Parser<Charac
         } else {
             return nil
         }
+    })
+}
+
+public func satisfyRange(_ a: Character, _ z: Character) -> Parser<Character> {
+    return satisfy({
+        return a <= $0 && $0 <= z
     })
 }
 
@@ -56,4 +84,44 @@ public func string(_ str: String) -> Parser<String> {
             return nil
         }
     })
+}
+
+public func octDigit() -> Parser<Character> {
+    return satisfy(isOctDigit(_:))
+}
+
+public func hexDigit() -> Parser<Character> {
+    return satisfy(isHexDigit(_:))
+}
+
+public func digit() -> Parser<Character> {
+    return satisfy(isDigit(_:))
+}
+
+public func letter() -> Parser<Character> {
+    return satisfy(isAlpha(_:))
+}
+
+public func alphaNum() -> Parser<Character> {
+    return satisfy(isAlphaNum(_:))
+}
+
+public func lower() -> Parser<Character> {
+    return satisfy(isLower(_:))
+}
+
+public func upper() -> Parser<Character> {
+    return satisfy(isUpper(_:))
+}
+
+public func tab() -> Parser<Character> {
+    return satisfy(isChar("\t"))
+}
+
+public func newline() -> Parser<Character> {
+    return satisfy(isChar("\n"))
+}
+
+public func space() -> Parser<Character> {
+    return satisfy(isChar(" "))
 }
--- Tests/SwiftyParsersTests/CharacterTests.swift
+++ Tests/SwiftyParsersTests/CharacterTests.swift
@@ -16,91 +16,6 @@ final class CharacterTests: XCTestCase {
         XCTAssertEqual(result?.1, "bc")
     }
 
-    func testIsDigit1() {
-        XCTAssertTrue(isDigit("0"))
-        XCTAssertTrue(isDigit("1"))
-        XCTAssertTrue(isDigit("2"))
-        XCTAssertTrue(isDigit("3"))
-        XCTAssertTrue(isDigit("4"))
-        XCTAssertTrue(isDigit("5"))
-        XCTAssertTrue(isDigit("6"))
-        XCTAssertTrue(isDigit("7"))
-        XCTAssertTrue(isDigit("8"))
-        XCTAssertTrue(isDigit("9"))
-    }
-
-    func testIsDigit2() {
-        XCTAssertFalse(isDigit("a"))
-    }
-
-    func testIsLower1() {
-        XCTAssertTrue(isLower("a"))
-        XCTAssertTrue(isLower("b"))
-        XCTAssertTrue(isLower("c"))
-        XCTAssertTrue(isLower("d"))
-        XCTAssertTrue(isLower("e"))
-        XCTAssertTrue(isLower("f"))
-        XCTAssertTrue(isLower("g"))
-        XCTAssertTrue(isLower("h"))
-        XCTAssertTrue(isLower("i"))
-        XCTAssertTrue(isLower("j"))
-        XCTAssertTrue(isLower("k"))
-        XCTAssertTrue(isLower("l"))
-        XCTAssertTrue(isLower("m"))
-        XCTAssertTrue(isLower("n"))
-        XCTAssertTrue(isLower("o"))
-        XCTAssertTrue(isLower("p"))
-        XCTAssertTrue(isLower("q"))
-        XCTAssertTrue(isLower("r"))
-        XCTAssertTrue(isLower("s"))
-        XCTAssertTrue(isLower("t"))
-        XCTAssertTrue(isLower("u"))
-        XCTAssertTrue(isLower("v"))
-        XCTAssertTrue(isLower("w"))
-        XCTAssertTrue(isLower("x"))
-        XCTAssertTrue(isLower("y"))
-        XCTAssertTrue(isLower("z"))
-    }
-
-    func testIsLower2() {
-        XCTAssertFalse(isLower("A"))
-        XCTAssertFalse(isLower("1"))
-    }
-
-    func testIsUpper1() {
-        XCTAssertTrue(isUpper("A"))
-        XCTAssertTrue(isUpper("B"))
-        XCTAssertTrue(isUpper("C"))
-        XCTAssertTrue(isUpper("D"))
-        XCTAssertTrue(isUpper("E"))
-        XCTAssertTrue(isUpper("F"))
-        XCTAssertTrue(isUpper("G"))
-        XCTAssertTrue(isUpper("H"))
-        XCTAssertTrue(isUpper("I"))
-        XCTAssertTrue(isUpper("J"))
-        XCTAssertTrue(isUpper("K"))
-        XCTAssertTrue(isUpper("L"))
-        XCTAssertTrue(isUpper("M"))
-        XCTAssertTrue(isUpper("N"))
-        XCTAssertTrue(isUpper("O"))
-        XCTAssertTrue(isUpper("P"))
-        XCTAssertTrue(isUpper("Q"))
-        XCTAssertTrue(isUpper("R"))
-        XCTAssertTrue(isUpper("S"))
-        XCTAssertTrue(isUpper("T"))
-        XCTAssertTrue(isUpper("U"))
-        XCTAssertTrue(isUpper("V"))
-        XCTAssertTrue(isUpper("W"))
-        XCTAssertTrue(isUpper("X"))
-        XCTAssertTrue(isUpper("Y"))
-        XCTAssertTrue(isUpper("Z"))
-    }
-
-    func testIsUpper2() {
-        XCTAssertFalse(isUpper("a"))
-        XCTAssertFalse(isUpper("1"))
-    }
-
     func testChar1() {
         let result = char("a").parse("abc")
 
@@ -157,15 +72,23 @@ final class CharacterTests: XCTestCase {
         XCTAssertNil(result)
     }
 
+    func testOctDigit1() {
+        let result = octDigit().parse("012")
+
+        XCTAssertNotNil(result)
+        XCTAssertEqual(result?.0, "0")
+        XCTAssertEqual(result?.1, "12")
+    }
+
+    func testOctDigit2() {
+        let result = octDigit().parse("a")
+
+        XCTAssertNil(result)
+    }
+
     static var allTests = [
         ("testSeparete1", testSeparete1),
         ("testSeparete2", testSeparete2),
-        ("testIsDigit1", testIsDigit1),
-        ("testIsDigit2", testIsDigit2),
-        ("testIsLower1", testIsLower1),
-        ("testIsLower2", testIsLower2),
-        ("testIsUpper1", testIsUpper1),
-        ("testIsUpper2", testIsUpper2),
         ("testChar1", testChar1),
         ("testChar2", testChar2),
         ("testNotChar1", testNotChar1),
@@ -174,5 +97,7 @@ final class CharacterTests: XCTestCase {
         ("testAny2", testAny2),
         ("testString1", testString1),
         ("testString2", testString2),
+        ("testOctDigit1", testOctDigit1),
+        ("testOctDigit2", testOctDigit2),
         ]
 }
