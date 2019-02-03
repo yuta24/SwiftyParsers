184f89e6f5b45a079679a09564c8df54cc2f0a1e
yuta24 <yuta24@bivre.com>
2019-02-03 18:25:57 +0900

    Impl Parser

--- Sources/SwiftyParsers/Parser.swift
+++ Sources/SwiftyParsers/Parser.swift
@@ -27,7 +27,7 @@ public extension Parser {
         }
     }
 
-    static func apply<B>(_ p: Parser<(A) -> B>, _ q: Parser<A>) -> Parser<B> {
+    static func seq<B>(_ p: Parser<(A) -> B>, _ q: Parser<A>) -> Parser<B> {
         return Parser<B> {
             if let (x, xs) = p.parse($0) {
                 if let r = q.parse(xs) {
@@ -39,5 +39,57 @@ public extension Parser {
                 return nil
             }
         }
+    }
+
+    static func seqDiscardRight<B>(_ p: Parser<A>, _ q: Parser<B>) -> Parser<A> {
+        return Parser<A> {
+            if let r1 = p.parse($0), let r2 = q.parse(r1.1) {
+                return (r1.0, r2.1)
+            } else {
+                return nil
+            }
+        }
+    }
+
+    static func seqDiscardLeft<B>(_ p: Parser<A>, _ q: Parser<B>) -> Parser<B> {
+        return Parser<B> {
+            if let r1 = p.parse($0), let r2 = q.parse(r1.1) {
+                return r2
+            } else {
+                return nil
+            }
+        }
+    }
+}
+
+public extension Parser {
+    static func bind<B>(_ p: Parser<A>, _ f: @escaping (A) -> Parser<B>) -> Parser<B> {
+        return Parser<B> {
+            if let r = p.parse($0) {
+                return f(r.0).parse(r.1)
+            } else {
+                return nil
+            }
+        }
+    }
+}
+
+public extension Parser {
+    static func choice(_ p: Parser<A>, _ q: Parser<A>) -> Parser<A> {
+        return Parser {
+            return p.parse($0) ?? q.parse($0)
+        }
+    }
+
+    static func some(_ p: Parser<A>) -> Parser<[A]> {
+        return bind(p, { (xs) -> Parser<[A]> in
+            return many(p)
+        })
+    }
+
+    static func many(_ p: Parser<A>) -> Parser<[A]> {
+        return Parser<[A]>.choice(
+            Parser<A>.some(p),
+            Parser<[A]>.pure([]))
     }
 }
