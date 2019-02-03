a20f6c7e79c1375493295bb7c9a147037b118697
yuta24 <yuta24@bivre.com>
2019-02-04 08:37:54 +0900

    Add test case for alternative

--- Tests/SwiftyParsersTests/ParserTests.swift
+++ Tests/SwiftyParsersTests/ParserTests.swift
@@ -2,10 +2,19 @@ import XCTest
 @testable import SwiftyParsers
 
 final class ParserTests: XCTestCase {
-    func testFunctor1() {
-        let p1 = Parser<Character> {
-            return separate($0)
+    let cp = Parser<Character> {
+        return separate($0)
+    }
+    let ip = Parser<Int> {
+        if let (x, xs) = separate($0), let i = Int(String(x)) {
+            return (i, xs)
+        } else {
+            return nil
         }
+    }
+
+    func testFunctor1() {
+        let p1 = cp
         let p2 = fmap(p1) { (c) -> Int in
             return Int(String(c)) ?? 0
         }
@@ -26,9 +35,7 @@ final class ParserTests: XCTestCase {
     }
 
     func testApplicative2() {
-        let p1 = Parser<Character> {
-            return separate($0)
-        }
+        let p1 = cp
         let p2 = Parser<(Character) -> Int> {
             if let (x, xs) = separate($0), let _ = Int(String(x)) {
                 return ({ Int(String($0)) ?? 0  }, xs)
@@ -45,16 +52,8 @@ final class ParserTests: XCTestCase {
     }
 
     func testApplicative3() {
-        let p1 = Parser<Character> {
-            return separate($0)
-        }
-        let p2 = Parser<Int> {
-            if let (x, xs) = separate($0), let i = Int(String(x)) {
-                return (i, xs)
-            } else {
-                return nil
-            }
-        }
+        let p1 = cp
+        let p2 = ip
         let p3 = seqDiscardRight(p1, p2)
 
         let r = p3.parse("a1c")
@@ -64,16 +63,8 @@ final class ParserTests: XCTestCase {
     }
 
     func testApplicative4() {
-        let p1 = Parser<Character> {
-            return separate($0)
-        }
-        let p2 = Parser<Int> {
-            if let (x, xs) = separate($0), let i = Int(String(x)) {
-                return (i, xs)
-            } else {
-                return nil
-            }
-        }
+        let p1 = cp
+        let p2 = ip
         let p3 = seqDiscardLeft(p1, p2)
 
         let r = p3.parse("a1c")
@@ -83,24 +74,36 @@ final class ParserTests: XCTestCase {
     }
 
     func testMonad1() {
-        let p1 = Parser<Character> {
-            return separate($0)
-        }
+        let p1 = cp
         let p2 = { (c: Character) -> Parser<Int> in
-            return Parser<Int> {
-                if let (x, xs) = separate($0), let i = Int(String(x)) {
-                    return (i, xs)
-                } else {
-                    return nil
-                }
-            }
+            return pure(Int(String(c)) ?? 0)
         }
         let p3 = flatMap(p1, p2)
 
         let r = p3.parse("123")
         XCTAssertNotNil(3)
-        XCTAssertEqual(r?.0, 2)
-        XCTAssertEqual(r?.1, "3")
+        XCTAssertEqual(r?.0, 1)
+        XCTAssertEqual(r?.1, "23")
+    }
+
+    func testAlternative1() {
+        enum Tuple {
+            case left
+            case right
+        }
+        let p1 = fmap(ip, { _ in Tuple.left })
+        let p2 = fmap(cp, { _ in Tuple.right })
+        let p3 = choice(p1, p2)
+
+        let r1 = p3.parse("123")
+        XCTAssertNotNil(r1)
+        XCTAssertEqual(r1?.0, .left)
+        XCTAssertEqual(r1?.1, "23")
+
+        let r2 = p3.parse("abc")
+        XCTAssertNotNil(r2)
+        XCTAssertEqual(r2?.0, .right)
+        XCTAssertEqual(r2?.1, "bc")
     }
 
 // TODO: For linux
