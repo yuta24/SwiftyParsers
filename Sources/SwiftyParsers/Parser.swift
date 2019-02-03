30eb7c053ce3efb80bb1cb06af777eb5bc5007ea
yuta24 <yuta24@bivre.com>
2019-02-04 01:11:47 +0900

    Add test cases

--- Sources/SwiftyParsers/Parser.swift
+++ Sources/SwiftyParsers/Parser.swift
@@ -8,68 +8,65 @@ public struct Parser<A> {
     let parse: (String) -> (A, String)?
 }
 
-public extension Parser {
-    static func fmap<B>(_ p: Parser<A>, _ f: @escaping (A) -> B) -> Parser<B> {
-        return Parser<B> {
-            if let r = p.parse($0) {
-                return (f(r.0), r.1)
-            } else {
-                return nil
-            }
+// MARK: - Functor
+func fmap<A, B>(_ p: Parser<A>, _ f: @escaping (A) -> B) -> Parser<B> {
+    return Parser<B> {
+        if let r = p.parse($0) {
+            return (f(r.0), r.1)
+        } else {
+            return nil
         }
     }
 }
 
-public extension Parser {
-    static func pure(_ a: A) -> Parser<A> {
-        return Parser {
-            return (a, $0)
-        }
+// MARK: - Applicative
+func pure<A>(_ a: A) -> Parser<A> {
+    return Parser {
+        return (a, $0)
     }
+}
 
-    static func seq<B>(_ p: Parser<(A) -> B>, _ q: Parser<A>) -> Parser<B> {
-        return Parser<B> {
-            if let (x, xs) = p.parse($0) {
-                if let r = q.parse(xs) {
-                    return (x(r.0), r.1)
-                } else {
-                    return nil
-                }
+func seq<A, B>(_ p: Parser<(A) -> B>, _ q: Parser<A>) -> Parser<B> {
+    return Parser<B> {
+        if let (x, xs) = p.parse($0) {
+            if let r = q.parse(xs) {
+                return (x(r.0), r.1)
             } else {
                 return nil
             }
-        }
-    }
-
-    static func seqDiscardRight<B>(_ p: Parser<A>, _ q: Parser<B>) -> Parser<A> {
-        return Parser<A> {
-            if let r1 = p.parse($0), let r2 = q.parse(r1.1) {
-                return (r1.0, r2.1)
-            } else {
-                return nil
-            }
-        }
-    }
-
-    static func seqDiscardLeft<B>(_ p: Parser<A>, _ q: Parser<B>) -> Parser<B> {
-        return Parser<B> {
-            if let r1 = p.parse($0), let r2 = q.parse(r1.1) {
-                return r2
-            } else {
-                return nil
-            }
+        } else {
+            return nil
         }
     }
 }
 
-public extension Parser {
-    static func bind<B>(_ p: Parser<A>, _ f: @escaping (A) -> Parser<B>) -> Parser<B> {
-        return Parser<B> {
-            if let r = p.parse($0) {
-                return f(r.0).parse(r.1)
-            } else {
-                return nil
-            }
+func seqDiscardRight<A, B>(_ p: Parser<A>, _ q: Parser<B>) -> Parser<A> {
+    return Parser<A> {
+        if let r1 = p.parse($0), let r2 = q.parse(r1.1) {
+            return (r1.0, r2.1)
+        } else {
+            return nil
+        }
+    }
+}
+
+func seqDiscardLeft<A, B>(_ p: Parser<A>, _ q: Parser<B>) -> Parser<B> {
+    return Parser<B> {
+        if let r1 = p.parse($0), let r2 = q.parse(r1.1) {
+            return r2
+        } else {
+            return nil
+        }
+    }
+}
+
+// MARK: - Monad
+func flatMap<A, B>(_ p: Parser<A>, _ f: @escaping (A) -> Parser<B>) -> Parser<B> {
+    return Parser<B> {
+        if let r = p.parse($0) {
+            return f(r.0).parse(r.1)
+        } else {
+            return nil
         }
     }
 }
@@ -82,7 +79,7 @@ public extension Parser {
     }
 
     static func some(_ p: Parser<A>) -> Parser<[A]> {
-        return bind(p, { (xs) -> Parser<[A]> in
+        return flatMap(p, { (xs) -> Parser<[A]> in
             return many(p)
         })
     }
@@ -90,6 +87,6 @@ public extension Parser {
     static func many(_ p: Parser<A>) -> Parser<[A]> {
         return Parser<[A]>.choice(
             Parser<A>.some(p),
-            Parser<[A]>.pure([]))
+            pure([]))
     }
 }
--- Tests/SwiftyParsersTests/ParserTests.swift
+++ Tests/SwiftyParsersTests/ParserTests.swift
@@ -2,14 +2,73 @@ import XCTest
 @testable import SwiftyParsers
 
 final class ParserTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-//        XCTAssertEqual(Parser(), "Hello, World!")
+    func testMonadic1() {
+        let p1 = Parser<Character> {
+            return separete($0)
+        }
+        let p2 = fmap(p1) { (c) -> Int in
+            return Int(String(c)) ?? 0
+        }
+
+        let r = p2.parse("123")
+        XCTAssertNotNil(r)
+        XCTAssertEqual(r?.0, 1)
+        XCTAssertEqual(r?.1, "23")
+    }
+
+    func testMonadic2() {
+        let p1: Parser<Character> = pure("a")
+
+        let r = p1.parse("123")
+        XCTAssertNotNil(r)
+        XCTAssertEqual(r?.0, "a")
+        XCTAssertEqual(r?.1, "123")
+    }
+
+    func testMonadic3() {
+        let p1 = Parser<Character> {
+            return separete($0)
+        }
+        let p2 = Parser<(Character) -> Int> {
+            if let (x, xs) = separete($0), let _ = Int(String(x)) {
+                return ({ Int(String($0)) ?? 0  }, xs)
+            } else {
+                return nil
+            }
+        }
+        let p3 = seq(p2, p1)
+
+        let r = p3.parse("123")
+        XCTAssertNotNil(r)
+        XCTAssertEqual(r?.0, 2)
+        XCTAssertEqual(r?.1, "3")
+    }
+
+    func testMonadic4() {
+        let p1 = Parser<Character> {
+            return separete($0)
+        }
+        let p2 = { (c: Character) -> Parser<Int> in
+            return Parser<Int> {
+                if let (x, xs) = separete($0), let i = Int(String(x)) {
+                    return (i, xs)
+                } else {
+                    return nil
+                }
+            }
+        }
+        let p3 = flatMap(p1, p2)
+
+        let r = p3.parse("123")
+        XCTAssertNotNil(3)
+        XCTAssertEqual(r?.0, 2)
+        XCTAssertEqual(r?.1, "3")
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testMonadic1", testMonadic1),
+        ("testMonadic2", testMonadic2),
+        ("testMonadic3", testMonadic3),
+        ("testMonadic4", testMonadic4),
     ]
 }
